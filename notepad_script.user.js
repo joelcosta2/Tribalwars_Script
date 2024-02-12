1676cf1854ccdf86ad34967dee6613f611a72099
joelcosta2 <joelcosta95@hotmail.com>
2024-02-12 22:26:55 +0000

    test split files

--- notepad_script.user.js
+++ notepad_script.user.js
@@ -1,133 +1,130 @@
-(function () {
-    'use strict';
 
-    // Notepad Module
-    function loadNote() {
-        var cookieNotepadJson = getCookie('vilagges_notepad');
-        var notepadArray = cookieNotepadJson ? JSON.parse(cookieNotepadJson) : [];
-        var textToShow = notepadArray[currentVillageIndex];
+// Notepad Module
+function loadNote() {
+    var cookieNotepadJson = getCookie('vilagges_notepad');
+    var notepadArray = cookieNotepadJson ? JSON.parse(cookieNotepadJson) : [];
+    var textToShow = notepadArray[currentVillageIndex];
 
-        if (textToShow !== '') {
-            // village_note_script show this
-            toggleElement('village_note_script');
-            //set text to village-note-body_script
-            var textPlacer = document.getElementById('village-note-body_script');
-            textPlacer.textContent = textToShow;
+    if (textToShow !== '') {
+        // village_note_script show this
+        toggleElement('village_note_script');
+        //set text to village-note-body_script
+        var textPlacer = document.getElementById('village-note-body_script');
+        textPlacer.textContent = textToShow;
 
-        }
     }
+}
 
-    function saveNote() {
-        var textToSave = document.getElementById('message_note_script').value;
-        var currentCookieValue = getCookie('vilagges_notepad');
-        var notepadArray = currentCookieValue ? JSON.parse(currentCookieValue) : [];
-        notepadArray[currentVillageIndex] = textToSave;
-        var jsonToSave = JSON.stringify(notepadArray);
-        setCookie('vilagges_notepad', jsonToSave, 100000000000000);
-        toggleElement('note_body_edit');
+function saveNote() {
+    var textToSave = document.getElementById('message_note_script').value;
+    var currentCookieValue = getCookie('vilagges_notepad');
+    var notepadArray = currentCookieValue ? JSON.parse(currentCookieValue) : [];
+    notepadArray[currentVillageIndex] = textToSave;
+    var jsonToSave = JSON.stringify(notepadArray);
+    setCookie('vilagges_notepad', jsonToSave, 100000000000000);
+    toggleElement('note_body_edit');
+    loadNote();
+    toggleElement('edit_notepad_link_script');
+}
+
+function openEditModeNote() {
+    listenTextAreas();
+    var currentCookieValue = getCookie('vilagges_notepad');
+    var notepadArray = currentCookieValue ? JSON.parse(currentCookieValue) : [];
+    document.getElementById('message_note_script').value = notepadArray[currentVillageIndex];
+    toggleElement('note_body_edit');
+    document.getElementById('village_note_script').style.display = 'none';
+    document.getElementById('edit_notepad_link_script').style.display = 'none';
+}
+
+function injectNotepadOveriew(columnToUse) {
+    if (showNotepad) {
+        // Criação do botão "Editar"
+        var editLink = document.createElement('a');
+        editLink.id = 'edit_notepad_link_script';
+        editLink.textContent = '» Editar';
+        editLink.onclick = function () {
+            openEditModeNote();
+        };
+
+        // Criação da área de texto para a nota e do botão "Guardar"
+        var noteTextarea = document.createElement('textarea');
+        noteTextarea.id = 'message_note_script';
+        noteTextarea.name = 'note';
+        noteTextarea.style.width = '97%';
+        noteTextarea.rows = '10';
+        noteTextarea.cols = '40';
+
+        var saveButton = document.createElement('a');
+        saveButton.id = 'note_submit_button_script';
+        saveButton.className = 'btn btn-default';
+        saveButton.textContent = 'Guardar';
+        saveButton.onclick = function () {
+            saveNote();
+        };
+
+        // Criação da estrutura da tabela
+        var table = document.createElement('table');
+        table.width = '100%';
+
+        var tbody = document.createElement('tbody');
+
+        // Criação da linha para mostrar a nota
+        var noteRow = document.createElement('tr');
+        noteRow.id = 'village_note_script';
+        noteRow.style.display = 'none';
+
+        var noteCell = document.createElement('td');
+        var noteDiv = document.createElement('div');
+        noteDiv.className = 'village-note';
+        var noteBodyDiv = document.createElement('div');
+        noteBodyDiv.id = 'village-note-body_script';
+        noteBodyDiv.className = 'village-note-body';
+        noteBodyDiv.style.whiteSpace = 'pre-wrap';
+
+        noteDiv.appendChild(noteBodyDiv);
+        noteCell.appendChild(noteDiv);
+        noteRow.appendChild(noteCell);
+
+        // Criação da linha para editar a nota
+        var editRow = document.createElement('tr');
+        editRow.id = 'note_body_edit';
+        editRow.style.display = 'none';
+
+        var editCell = document.createElement('td');
+        var editDiv = document.createElement('div');
+        editDiv.className = 'village-note';
+        editDiv.style.width = '100%';
+        editDiv.style.overflow = 'hidden';
+
+        var textareaDiv = document.createElement('div');
+        textareaDiv.appendChild(noteTextarea);
+
+        var buttonDiv = document.createElement('div');
+        buttonDiv.appendChild(saveButton);
+
+        editDiv.appendChild(textareaDiv);
+        editDiv.appendChild(buttonDiv);
+
+        var editNoteBodyDiv = document.createElement('div');
+        editNoteBodyDiv.className = 'village-note-body';
+        editDiv.appendChild(editNoteBodyDiv);
+
+        editCell.appendChild(editDiv);
+        editRow.appendChild(editCell);
+
+        // Criação da linha para o botão "Editar"
+        var editButtonRow = document.createElement('tr');
+        var editButtonCell = document.createElement('td');
+        editButtonCell.appendChild(editLink);
+        editButtonRow.appendChild(editButtonCell);
+
+        tbody.appendChild(noteRow);
+        tbody.appendChild(editRow);
+        tbody.appendChild(editButtonRow);
+        table.appendChild(tbody);
+
+        createAssetElement('Notepad', table, columnToUse);
         loadNote();
-        toggleElement('edit_notepad_link_script');
     }
-
-    function openEditModeNote() {
-        listenTextAreas();
-        var currentCookieValue = getCookie('vilagges_notepad');
-        var notepadArray = currentCookieValue ? JSON.parse(currentCookieValue) : [];
-        document.getElementById('message_note_script').value = notepadArray[currentVillageIndex];
-        toggleElement('note_body_edit');
-        document.getElementById('village_note_script').style.display = 'none';
-        document.getElementById('edit_notepad_link_script').style.display = 'none';
-    }
-
-    function injectNotepadOveriew(columnToUse) {
-        if (showNotepad) {
-            // Criação do botão "Editar"
-            var editLink = document.createElement('a');
-            editLink.id = 'edit_notepad_link_script';
-            editLink.textContent = '» Editar';
-            editLink.onclick = function () {
-                openEditModeNote();
-            };
-
-            // Criação da área de texto para a nota e do botão "Guardar"
-            var noteTextarea = document.createElement('textarea');
-            noteTextarea.id = 'message_note_script';
-            noteTextarea.name = 'note';
-            noteTextarea.style.width = '97%';
-            noteTextarea.rows = '10';
-            noteTextarea.cols = '40';
-
-            var saveButton = document.createElement('a');
-            saveButton.id = 'note_submit_button_script';
-            saveButton.className = 'btn btn-default';
-            saveButton.textContent = 'Guardar';
-            saveButton.onclick = function () {
-                saveNote();
-            };
-
-            // Criação da estrutura da tabela
-            var table = document.createElement('table');
-            table.width = '100%';
-
-            var tbody = document.createElement('tbody');
-
-            // Criação da linha para mostrar a nota
-            var noteRow = document.createElement('tr');
-            noteRow.id = 'village_note_script';
-            noteRow.style.display = 'none';
-
-            var noteCell = document.createElement('td');
-            var noteDiv = document.createElement('div');
-            noteDiv.className = 'village-note';
-            var noteBodyDiv = document.createElement('div');
-            noteBodyDiv.id = 'village-note-body_script';
-            noteBodyDiv.className = 'village-note-body';
-            noteBodyDiv.style.whiteSpace = 'pre-wrap';
-
-            noteDiv.appendChild(noteBodyDiv);
-            noteCell.appendChild(noteDiv);
-            noteRow.appendChild(noteCell);
-
-            // Criação da linha para editar a nota
-            var editRow = document.createElement('tr');
-            editRow.id = 'note_body_edit';
-            editRow.style.display = 'none';
-
-            var editCell = document.createElement('td');
-            var editDiv = document.createElement('div');
-            editDiv.className = 'village-note';
-            editDiv.style.width = '100%';
-            editDiv.style.overflow = 'hidden';
-
-            var textareaDiv = document.createElement('div');
-            textareaDiv.appendChild(noteTextarea);
-
-            var buttonDiv = document.createElement('div');
-            buttonDiv.appendChild(saveButton);
-
-            editDiv.appendChild(textareaDiv);
-            editDiv.appendChild(buttonDiv);
-
-            var editNoteBodyDiv = document.createElement('div');
-            editNoteBodyDiv.className = 'village-note-body';
-            editDiv.appendChild(editNoteBodyDiv);
-
-            editCell.appendChild(editDiv);
-            editRow.appendChild(editCell);
-
-            // Criação da linha para o botão "Editar"
-            var editButtonRow = document.createElement('tr');
-            var editButtonCell = document.createElement('td');
-            editButtonCell.appendChild(editLink);
-            editButtonRow.appendChild(editButtonCell);
-
-            tbody.appendChild(noteRow);
-            tbody.appendChild(editRow);
-            tbody.appendChild(editButtonRow);
-            table.appendChild(tbody);
-
-            createAssetElement('Notepad', table, columnToUse);
-            loadNote();
-        }
-    }
-})();
+}
--- utils.user.js
+++ utils.user.js
@@ -1,208 +1,205 @@
-(function () {
-    'use strict';
 
-    //Utils
-    function setCookieCurrentVillage() {
-        var str = currentURL;
-        var temp = str.indexOf("="),
-            temp2 = str.indexOf("&", temp),
-            villageID = str.slice(temp + 1, temp2);
+//Utils
+function setCookieCurrentVillage() {
+    var str = currentURL;
+    var temp = str.indexOf("="),
+        temp2 = str.indexOf("&", temp),
+        villageID = str.slice(temp + 1, temp2);
 
-        var i = 0,
-            villgersNum = sizeOfObject(villageList);
+    var i = 0,
+        villgersNum = sizeOfObject(villageList);
 
-        for (i = 0; i < villgersNum; i++) {
-            var urlTemp = villageList[i].url;
-            if (urlTemp.includes(villageID)) {
-                currentVillageIndex = i;
-                setCookie('current_village', currentVillageIndex, 1000000000000);
-                return;
-            }
+    for (i = 0; i < villgersNum; i++) {
+        var urlTemp = villageList[i].url;
+        if (urlTemp.includes(villageID)) {
+            currentVillageIndex = i;
+            setCookie('current_village', currentVillageIndex, 1000000000000);
+            return;
         }
     }
+}
 
-    function toggleElement(element) {
-        var elementToToggle = document.getElementById(element);
-        if (elementToToggle) {
-            if (elementToToggle.style.display === 'none') {
-                elementToToggle.style.display = 'contents';
-            } else {
-                elementToToggle.style.display = 'none';
-            }
-        }
-    }
-
-    function togglePopUp(className) {
-        var elementToToggle = document.getElementsByClassName(className)[0];
+function toggleElement(element) {
+    var elementToToggle = document.getElementById(element);
+    if (elementToToggle) {
         if (elementToToggle.style.display === 'none') {
-            elementToToggle.style.display = 'block';
+            elementToToggle.style.display = 'contents';
         } else {
             elementToToggle.style.display = 'none';
         }
     }
+}
 
-    function listenTextAreas() {
-        // get all inputs || textareas
-        var textAreas = document.getElementsByTagName('textarea');
-        var i = 0;
-        for (i = 0; i < textAreas.length; i++) {
-            textAreas[i].onfocus = function () { textSelected = true };
-            textAreas[i].onblur = function () { textSelected = false };
-        }
-        textAreas = document.getElementsByTagName('input');
-        for (i = 0; i < textAreas.length; i++) {
-            textAreas[i].onfocus = function () { textSelected = true };
-            textAreas[i].onblur = function () { textSelected = false };
-        }
+function togglePopUp(className) {
+    var elementToToggle = document.getElementsByClassName(className)[0];
+    if (elementToToggle.style.display === 'none') {
+        elementToToggle.style.display = 'block';
+    } else {
+        elementToToggle.style.display = 'none';
     }
+}
 
-    function defineKeyboardShortcuts() {
-        if (usarAtalhosTrocarAldeias) {
-            $(document).keydown(function (evt) {
-                if (evt.keyCode == 65 && !textSelected) {
-                    evt.preventDefault();
-                    previousVillage();
-                }
-            });
-            $(document).keydown(function (evt) {
-                if (evt.keyCode == 68 && !textSelected) {
-                    evt.preventDefault();
-                    nextVillage();
-                }
-            });
-        }
+function listenTextAreas() {
+    // get all inputs || textareas
+    var textAreas = document.getElementsByTagName('textarea');
+    var i = 0;
+    for (i = 0; i < textAreas.length; i++) {
+        textAreas[i].onfocus = function () { textSelected = true };
+        textAreas[i].onblur = function () { textSelected = false };
     }
+    textAreas = document.getElementsByTagName('input');
+    for (i = 0; i < textAreas.length; i++) {
+        textAreas[i].onfocus = function () { textSelected = true };
+        textAreas[i].onblur = function () { textSelected = false };
+    }
+}
 
-    function getCookie(cname) {
-        var name = cname + "=";
-        var ca = document.cookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) == ' ') {
-                c = c.substring(1);
+function defineKeyboardShortcuts() {
+    if (usarAtalhosTrocarAldeias) {
+        $(document).keydown(function (evt) {
+            if (evt.keyCode == 65 && !textSelected) {
+                evt.preventDefault();
+                previousVillage();
             }
-            if (c.indexOf(name) == 0) {
-                return c.substring(name.length, c.length);
+        });
+        $(document).keydown(function (evt) {
+            if (evt.keyCode == 68 && !textSelected) {
+                evt.preventDefault();
+                nextVillage();
             }
+        });
+    }
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
         }
-        return "";
-    }
-
-    function setCookie(cname, cvalue, exdays) {
-        var d = new Date();
-        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-        var expires = "expires=" + d.toUTCString();
-        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    }
-
-    function sizeOfObject(obj) {
-        var size = 0, key;
-        for (key in obj) {
-            if (obj.hasOwnProperty(key)) size++;
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
         }
-        return size;
     }
+    return "";
+}
 
-    function createAssetElement(title, contents, columnToUse, extra_name) {
-        var columnElement = document.getElementById(columnToUse);
-        var elemId = title.toLowerCase().replace(/ /g, '_'); // Obtém o título do elemento
-        var elemName = extra_name ? elemId + '_' + extra_name : elemId;
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 
-        // Create a container div
-        var containerDiv = document.createElement('div');
-        containerDiv.id = 'show_' + elemName;
-        containerDiv.className = 'vis moveable widget';
+function sizeOfObject(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+}
 
-        // Create the header with button
-        var header = document.createElement('h4');
-        header.className = 'head with-button';
-        header.textContent = title;
-        header.style.minWidth = 'max-content';
-        header.style.width = '-webkit-fill-available';
-        header.style.cursor = 'pointer';
-        header.onclick = function () {
-            var content = document.getElementById('widget_content_' + elemName);
-            var miniElem = document.getElementById('mini_' + elemName);
-            content.style.display = content.style.display === 'none' ? 'block' : 'none';
-            if (miniElem.src.includes('minus')) {
-                miniElem.src = miniElem.src.replace('minus', 'plus');
-                // Encontra o objeto correspondente no array de assets e atualiza o valor de 'open' para false
-                settings_cookies.assets.find(asset => asset.name === elemId).open = false;
-            } else {
-                miniElem.src = miniElem.src.replace('plus', 'minus');
-                // Encontra o objeto correspondente no array de assets e atualiza o valor de 'open' para true
-                settings_cookies.assets.find(asset => asset.name === elemId).open = true;
-            }
+function createAssetElement(title, contents, columnToUse, extra_name) {
+    var columnElement = document.getElementById(columnToUse);
+    var elemId = title.toLowerCase().replace(/ /g, '_'); // Obtém o título do elemento
+    var elemName = extra_name ? elemId + '_' + extra_name : elemId;
 
-            setCookie('settings_cookies', JSON.stringify(settings_cookies), 100000000);
-        };
+    // Create a container div
+    var containerDiv = document.createElement('div');
+    containerDiv.id = 'show_' + elemName;
+    containerDiv.className = 'vis moveable widget';
 
-
-        // Create the image for the button
-        var buttonImg = document.createElement('img');
-        buttonImg.className = 'widget-button';
-        buttonImg.src = settings_cookies.assets.find(asset => asset.name === elemId).open ? 'graphic/minus.png' : 'graphic/plus.png';
-        buttonImg.id = 'mini_' + elemName;
-        header.appendChild(buttonImg);
-
-        var contentDiv = document.createElement('div');
-        contentDiv.id = 'widget_content_' + elemName;
-        contentDiv.style.display = settings_cookies.assets.find(asset => asset.name === elemId).open ? 'block' : 'none';
-        contentDiv.appendChild(contents);
-        containerDiv.appendChild(header);
-        containerDiv.appendChild(contentDiv);
-
-        var assetIndex = settings_cookies.assets.find(asset => asset.name === elemId).pos;
-        var children = columnElement.childNodes;
-        // Verifica se o índice fornecido é válido
-        if (assetIndex >= 0 && assetIndex <= children.length) {
-            var refChild = (assetIndex + 1 === children.length) ? null : children[assetIndex + 1];
-            columnElement.insertBefore(containerDiv, refChild);
+    // Create the header with button
+    var header = document.createElement('h4');
+    header.className = 'head with-button';
+    header.textContent = title;
+    header.style.minWidth = 'max-content';
+    header.style.width = '-webkit-fill-available';
+    header.style.cursor = 'pointer';
+    header.onclick = function () {
+        var content = document.getElementById('widget_content_' + elemName);
+        var miniElem = document.getElementById('mini_' + elemName);
+        content.style.display = content.style.display === 'none' ? 'block' : 'none';
+        if (miniElem.src.includes('minus')) {
+            miniElem.src = miniElem.src.replace('minus', 'plus');
+            // Encontra o objeto correspondente no array de assets e atualiza o valor de 'open' para false
+            settings_cookies.assets.find(asset => asset.name === elemId).open = false;
         } else {
-            columnElement.appendChild(containerDiv);
+            miniElem.src = miniElem.src.replace('plus', 'minus');
+            // Encontra o objeto correspondente no array de assets e atualiza o valor de 'open' para true
+            settings_cookies.assets.find(asset => asset.name === elemId).open = true;
         }
-
-        //columnElement.appendChild(containerDiv);
-    }
-
-    function saveColumnOrder() {
-        var childDivs = Array.from(document.getElementById('script_column').children);
-        var childDivIds = childDivs.map(function (div) {
-            return div.id.replace('show_', '');
-        });
-
-        childDivIds.forEach(function (id, index) {
-            var asset = settings_cookies.assets.find(function (asset) {
-                return asset.name === id;
-            });
-            if (asset) {
-                asset.pos = index;
-            }
-        });
-
-        var parentIds = [RIGHT_COLUMN, LEFT_COLUMN, CENTER_COLUMN];
-        parentIds.forEach(function (parentId) {
-            // Obtém o elemento pai pelo ID
-            var parentElement = document.getElementById(parentId);
-            if (parentElement) {
-                // Obtém os elementos filhos do pai
-                var children = parentElement.children;
-                // Itera sobre os elementos filhos
-                for (var i = 0; i < children.length; i++) {
-                    var childId = children[i].id.replace("show_", "");
-                    // Verifica se o ID do filho está presente em settings_cookies.assets
-                    var assetIndex = settings_cookies.assets.findIndex(function (asset) {
-                        return asset.name === childId;
-                    });
-                    if (assetIndex !== -1) {
-                        // Atualiza a coluna com o ID do pai e a posição com a posição do filho no HTML
-                        settings_cookies.assets[assetIndex].column = parentId;
-                        settings_cookies.assets[assetIndex].pos = i;
-                    }
-                }
-            }
-        });
 
         setCookie('settings_cookies', JSON.stringify(settings_cookies), 100000000);
+    };
+
+
+    // Create the image for the button
+    var buttonImg = document.createElement('img');
+    buttonImg.className = 'widget-button';
+    buttonImg.src = settings_cookies.assets.find(asset => asset.name === elemId).open ? 'graphic/minus.png' : 'graphic/plus.png';
+    buttonImg.id = 'mini_' + elemName;
+    header.appendChild(buttonImg);
+
+    var contentDiv = document.createElement('div');
+    contentDiv.id = 'widget_content_' + elemName;
+    contentDiv.style.display = settings_cookies.assets.find(asset => asset.name === elemId).open ? 'block' : 'none';
+    contentDiv.appendChild(contents);
+    containerDiv.appendChild(header);
+    containerDiv.appendChild(contentDiv);
+
+    var assetIndex = settings_cookies.assets.find(asset => asset.name === elemId).pos;
+    var children = columnElement.childNodes;
+    // Verifica se o índice fornecido é válido
+    if (assetIndex >= 0 && assetIndex <= children.length) {
+        var refChild = (assetIndex + 1 === children.length) ? null : children[assetIndex + 1];
+        columnElement.insertBefore(containerDiv, refChild);
+    } else {
+        columnElement.appendChild(containerDiv);
     }
-})();
+
+    //columnElement.appendChild(containerDiv);
+}
+
+function saveColumnOrder() {
+    var childDivs = Array.from(document.getElementById('script_column').children);
+    var childDivIds = childDivs.map(function (div) {
+        return div.id.replace('show_', '');
+    });
+
+    childDivIds.forEach(function (id, index) {
+        var asset = settings_cookies.assets.find(function (asset) {
+            return asset.name === id;
+        });
+        if (asset) {
+            asset.pos = index;
+        }
+    });
+
+    var parentIds = [RIGHT_COLUMN, LEFT_COLUMN, CENTER_COLUMN];
+    parentIds.forEach(function (parentId) {
+        // Obtém o elemento pai pelo ID
+        var parentElement = document.getElementById(parentId);
+        if (parentElement) {
+            // Obtém os elementos filhos do pai
+            var children = parentElement.children;
+            // Itera sobre os elementos filhos
+            for (var i = 0; i < children.length; i++) {
+                var childId = children[i].id.replace("show_", "");
+                // Verifica se o ID do filho está presente em settings_cookies.assets
+                var assetIndex = settings_cookies.assets.findIndex(function (asset) {
+                    return asset.name === childId;
+                });
+                if (assetIndex !== -1) {
+                    // Atualiza a coluna com o ID do pai e a posição com a posição do filho no HTML
+                    settings_cookies.assets[assetIndex].column = parentId;
+                    settings_cookies.assets[assetIndex].pos = i;
+                }
+            }
+        }
+    });
+
+    setCookie('settings_cookies', JSON.stringify(settings_cookies), 100000000);
+}
